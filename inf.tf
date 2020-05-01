152392fedd19f44b60378d0aa69c4a0a127496b6
Reji V L <reji@example.com>
2020-05-01 11:55:32 +0530

    vnet setup

---
+++ inf.tf
@@ -0,0 +1,18 @@
+resource "azurerm_resource_group" "vnet_rg"{
+    name = "vnet-rg"
+    location = "East US 2"
+}
+
+resource "azurerm_virtual_network" "couch_net"{
+    name = "couch-vnet"
+    address_space = ["11.0.0.0/16"]
+    location = azurerm_resource_group.vnet_rg.location
+    resource_group_name = azurerm_resource_group.vnet_rg.name
+}
+
+resource "azurerm_subnet" "couch_sub"{
+    name = "couch-subnet"
+    resource_group_name = azurerm_resource_group.vnet_rg.name
+    virtual_network_name = azurerm_virtual_network.couch_net.name
+    address_space = "11.0.2.0/24"
+}
