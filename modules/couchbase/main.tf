e46e3a1c56b7fb4f6edd14cc8aa3c1164d9125f8
reji-vl <56560619+reji-vl@users.noreply.github.com>
2020-05-03 08:01:17 +0530

    Update main.tf
    update

--- modules/couchbase/main.tf
+++ modules/couchbase/main.tf
@@ -54,10 +54,10 @@ resource "azurerm_linux_virtual_machine" "couch_vm"{
       version   = "latest"
     }
   
-/*  provisioner "file" {
+  provisioner "file" {
     source      = "pre-provision.sh"
     destination = "/tmp/pre-provision.sh"
-  } */
+  } 
 
   provisioner "remote-exec" {
     connection {
@@ -66,11 +66,11 @@ resource "azurerm_linux_virtual_machine" "couch_vm"{
       user     =  var.username                                       
       password =  var.admin_pass                                   
     }
-    script = "${path.module}/pre-provision.sh}" 
+//    script = "${path.module}/pre-provision.sh}"  
     
-/*    inline = [
+    inline = [
       "chmod +x /tmp/pre-provision.sh",
       "/tmp/pre-provision.sh",
-    ]  */                       
+    ]                        
   }
 }
